a2459cf7f0f9582536b5947f2207fc9d33f8891e
agprincefu <agprincefu@tencent.com>
2018-01-29 22:40:25 +0800

    chapter7 learn over

---
+++ app/src/main/java/com/agprincefu/andriod/criminalintent/Crime.java
@@ -0,0 +1,53 @@
+package com.agprincefu.andriod.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by agprincefu on 2018/1/27.
+ */
+
+public class Crime {
+
+    private UUID mId;
+    private Date mDate;
+    private String mTitle;
+    private boolean mSolved;
+
+    public Crime(){
+        mId =  UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public void setId(UUID id) {
+        mId = id;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
