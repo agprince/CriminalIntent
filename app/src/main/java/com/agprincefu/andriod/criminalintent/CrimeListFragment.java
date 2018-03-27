eb14e113ca7cbb17fbb96c051c6e15993f7f69ea
agprincefu <agprincefu@tencent.com>
2018-03-27 23:13:36 +0800

    学习完成第三章

--- app/src/main/java/com/agprincefu/andriod/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/agprincefu/andriod/criminalintent/CrimeListFragment.java
@@ -1,17 +1,23 @@
 package com.agprincefu.andriod.criminalintent;
 
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import java.util.List;
 
@@ -24,9 +30,18 @@ public class CrimeListFragment extends Fragment {
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mCrimeAdapter;
 
+    private static final String SAVED_SUBTITLE_VISIBLE = "subtitle";
+
+    private boolean mSubtitleVisible;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(savedInstanceState!=null){
+            mSubtitleVisible = savedInstanceState.getBoolean(SAVED_SUBTITLE_VISIBLE);
+        }
+        setHasOptionsMenu(true);
     }
 
     @Nullable
@@ -57,9 +72,54 @@ public class CrimeListFragment extends Fragment {
         } else {
             mCrimeAdapter.notifyDataSetChanged();
         }
+        updateSubtitile();
 
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime_list, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.show_subtitle);
+        if(mSubtitleVisible){
+            menuItem.setTitle(R.string.hide_subtitle);
+        }else{
+            menuItem.setTitle(R.string.show_subtitle);
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.add_crime:
+                Crime crime = new Crime();
+                CrimeLab.get(getActivity()).addCrime(crime);
+                Intent intent = CrimePagerActivity.newIntent(getActivity(), crime.getId());
+                startActivity(intent);
+                return true;
+            case R.id.show_subtitle:
+                mSubtitleVisible = !mSubtitleVisible;
+
+                getActivity().invalidateOptionsMenu();
+                updateSubtitile();
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(SAVED_SUBTITLE_VISIBLE,mSubtitleVisible);
+    }
 
     private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
@@ -88,7 +148,7 @@ public class CrimeListFragment extends Fragment {
             //Toast.makeText(getActivity(),mCrime.getTitle(),Toast.LENGTH_SHORT).show();
             //Intent startCrime = new Intent(getActivity(),CrimeActivity.class);
             //Intent startCrime = CrimeActivity.newIntent(getActivity(), mCrime.getId());
-            Intent startCrime = CrimePagerActivity.newIntent(getActivity(),mCrime.getId());
+            Intent startCrime = CrimePagerActivity.newIntent(getActivity(), mCrime.getId());
             startActivity(startCrime);
         }
     }
@@ -119,5 +179,19 @@ public class CrimeListFragment extends Fragment {
             return mCrimes.size();
 
         }
+    }
+
+    private void updateSubtitile(){
+        int countCrime = CrimeLab.get(getActivity()).getCrimes().size();
+
+        String subTitle = getString(R.string.subtitle_format,countCrime);
+
+        if(!mSubtitleVisible){
+            subTitle =null;
+        }
+
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+
+        activity.getSupportActionBar().setSubtitle(subTitle);
     }
 }
--- app/src/main/java/com/agprincefu/andriod/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/agprincefu/andriod/criminalintent/CrimePagerActivity.java
@@ -59,4 +59,6 @@ public class CrimePagerActivity extends AppCompatActivity {
             }
         }
     }
+
+   
 }
