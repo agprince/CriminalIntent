f801a42e45a89c9ade273ee6fa4a43b6a584cf01
agprincefu <agprincefu@tencent.com>
2018-04-01 20:51:20 +0800

    学习完成第十五章

--- app/src/main/java/com/agprincefu/andriod/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/agprincefu/andriod/criminalintent/CrimeFragment.java
@@ -11,9 +11,11 @@ import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ShareCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +23,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Date;
 import java.util.UUID;
@@ -30,6 +33,8 @@ import java.util.UUID;
  */
 
 public class CrimeFragment extends Fragment {
+
+    private static final String TAG = "agtest";
 
     private static final String ARG_CRIME_ID = "crime_id";
     private static final String DATE_FRAGMENT_TAG = " date_fragment";
@@ -43,6 +48,7 @@ public class CrimeFragment extends Fragment {
     private CheckBox mSolvedCheckBox;
     private Button mSuspectButton;
     private Button mReportButton;
+    private Button mCallButton;
 
     public static CrimeFragment newInstance(UUID id) {
         Bundle bundle = new Bundle();
@@ -73,6 +79,7 @@ public class CrimeFragment extends Fragment {
         mSolvedCheckBox = v.findViewById(R.id.crime_solved);
         mSuspectButton = v.findViewById(R.id.crime_suspect);
         mReportButton = v.findViewById(R.id.crime_report);
+        mCallButton = v.findViewById(R.id.crime_call);
 
         mTitleField.setText(mCrime.getTitle());
         mSolvedCheckBox.setChecked(mCrime.isSolved());
@@ -139,12 +146,43 @@ public class CrimeFragment extends Fragment {
         mReportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_SEND);
-                intent.setType("text/plain");
-                intent.putExtra(Intent.EXTRA_TEXT,getCrimeReport());
-                intent.putExtra(Intent.EXTRA_SUBJECT,getString(R.string.crime_report_suspect));
-                intent = Intent.createChooser(intent,getString(R.string.send_report));
+                ShareCompat.IntentBuilder sb = ShareCompat.IntentBuilder.from(getActivity());
+                sb.setText(getCrimeReport()).setSubject(getString(R.string.send_report)).setType("text/plain").startChooser();
+
+               startActivity(sb.getIntent());
+
+
+
+
+               // Intent intent = new Intent(Intent.ACTION_SEND);
+               // intent.setType("text/plain");
+                //intent.putExtra(Intent.EXTRA_TEXT,getCrimeReport());
+                //intent.putExtra(Intent.EXTRA_SUBJECT,getString(R.string.crime_report_suspect));
+                //intent = Intent.createChooser(intent,getString(R.string.send_report));
+                //startActivity(intent);
+            }
+        });
+
+
+        mCallButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //分两步，第一步获得电话号码，第二步，用隐私的intent拨打电话
+
+                String name = mCrime.getSuspect();
+
+                if(name==null||"".equals(name)){
+                    Toast.makeText(getActivity(),"no suspect, so no phone namm to call",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String phoneNumber = getNumber(name);
+                Log.d(TAG, "phoneNumber: "+phoneNumber);
+                Uri uri = Uri.parse("tel:"+phoneNumber);
+                Log.d(TAG,"Uri : "+uri.toString());
+                Intent intent = new Intent(Intent.ACTION_DIAL,uri);
                 startActivity(intent);
+
+
             }
         });
 
@@ -207,4 +245,44 @@ public class CrimeFragment extends Fragment {
         String report = getString(R.string.crime_report, mCrime.getTitle(), dateString, solvedString, suspect);
         return report;
     }
+    private String getNumber(String name){
+
+        String number= null;
+
+        Uri uri  = ContactsContract.Contacts.CONTENT_URI;
+        String[] projection = new String[]{ContactsContract.Contacts._ID,ContactsContract.Contacts.DISPLAY_NAME};
+        Cursor cursor = getActivity().getContentResolver().query(uri,projection,null,null,null,null);
+        if(cursor!=null &&cursor.moveToFirst()){
+            do {
+                long id = cursor.getLong(0);
+                Log.d(TAG, "id:  "+id);
+                String cursorName = cursor.getString(1);
+                Log.d(TAG, "cursonNmae:  "+cursorName);
+
+                if(name.equals(cursorName)){
+
+                    String[] phoneProjection = new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+                    Cursor phonecursor = getActivity().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,phoneProjection,ContactsContract.CommonDataKinds.Phone.CONTACT_ID+"="+id,null,null);
+
+                    if(phonecursor!=null&& phonecursor.moveToFirst()){
+                        name  =phonecursor.getString(0);
+                        Log.d(TAG, "phoneName : "+name);
+                        return name;
+                    }
+
+
+
+                }
+
+            }while (cursor.moveToNext());
+        }
+
+
+
+
+        return number;
+
+    }
+
 }
