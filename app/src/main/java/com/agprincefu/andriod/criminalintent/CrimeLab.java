7e9f407840d0b0fe8844da86b59161811c968232
agprincefu <agprincefu@tencent.com>
2018-04-01 15:01:42 +0800

    十四章学习完成

--- app/src/main/java/com/agprincefu/andriod/criminalintent/CrimeLab.java
+++ app/src/main/java/com/agprincefu/andriod/criminalintent/CrimeLab.java
@@ -68,6 +68,12 @@ public class CrimeLab {
         }
 
     }
+    public void deleteCrime(Crime crime){
+        String uuidString = crime.getId().toString();
+
+        mDatabase.delete(CrimeTable.NAME,CrimeTable.Cols.UUID + " = ?",new String[]{uuidString});
+
+    }
 
     public List<Crime> getCrimes() {
 
